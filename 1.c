3d49fb3d63223a651a877b1c73d2b987cb320676
ags <manojyn.btech22@rvu.edu.in>
2023-08-28 12:56:38 +0530

    commit

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{
+int a,b,c;
+a=1;
+b=4;
+c=a+b;
+return c;
+} 
---
+++ git_1/git1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+int a=1;
+int b=3;
+int c;
+c=a+b;
+print("%d",c);
